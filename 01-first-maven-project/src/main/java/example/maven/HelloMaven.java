53749875deebfee98c008e8192b1bf4fbdea0b67
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-16 20:04:23 +0300

    logging with SFL4J & Logback

--- 01-first-maven-project/src/main/java/example/maven/HelloMaven.java
+++ 01-first-maven-project/src/main/java/example/maven/HelloMaven.java
@@ -1,7 +1,15 @@
 package example.maven;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+
+    private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello Maven!");
+        log.info("Info level");
+        log.debug("Debug level");
+        log.warn("Warn level");
     }
 }
