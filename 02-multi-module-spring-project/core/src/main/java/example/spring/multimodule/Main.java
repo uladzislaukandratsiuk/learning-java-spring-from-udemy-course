67293b4155e58d7f528d81337dddac8559febb42
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-17 21:05:39 +0300

    using spring container example

--- 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/Main.java
+++ 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/Main.java
@@ -2,12 +2,27 @@ package example.spring.multimodule;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private final static Logger log = LoggerFactory.getLogger(Main.class);
 
+    private final static String CONFIG_LOCATION = "beans.xml";
+
     public static void main(String[] args) {
+
         log.info("Guess the number game");
+
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        int number = numberGenerator.next();
+
+        log.info("number = {}", number);
+
+        context.close();
     }
 }
---
+++ 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/NumberGenerator.java
@@ -0,0 +1,8 @@
+package example.spring.multimodule;
+
+public interface NumberGenerator {
+
+    int next();
+
+    int getMaxNumber();
+}
