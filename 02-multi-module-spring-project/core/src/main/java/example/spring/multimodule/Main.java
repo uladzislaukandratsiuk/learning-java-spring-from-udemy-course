a27774bf7f4d5c66cad28a59919fdcc6c5d661c8
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-17 20:20:32 +0300

    creating multi-module project

---
+++ 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/Main.java
@@ -0,0 +1,13 @@
+package example.spring.multimodule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess the number game");
+    }
+}
