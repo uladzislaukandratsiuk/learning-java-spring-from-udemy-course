57abffadb772db8946d922a5e97e34f876f3376f
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-19 20:46:55 +0300

    setter based dependency injection

--- 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/GameImpl.java
+++ 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/GameImpl.java
@@ -16,7 +16,7 @@ public class GameImpl implements Game {
     private int remainingGuesses;
     private boolean validNumberRange = true;
 
-    public GameImpl(NumberGenerator numberGenerator) {
+    public void setNumberGenerator(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
     }
 
