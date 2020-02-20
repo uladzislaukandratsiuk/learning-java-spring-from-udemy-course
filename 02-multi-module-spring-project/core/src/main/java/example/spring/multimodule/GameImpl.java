f3327f3b0bae17bf09a77b13181c84d8da05f9dc
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-20 19:59:42 +0300

    Autowiring beans

--- 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/GameImpl.java
+++ 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/GameImpl.java
@@ -2,6 +2,7 @@ package example.spring.multimodule;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -10,7 +11,9 @@ public class GameImpl implements Game {
 
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
+    @Autowired
     private NumberGenerator numberGenerator;
+
     private int guessNumber = 10;
     private int number;
     private int guess;
@@ -33,10 +36,6 @@ public class GameImpl implements Game {
     @PreDestroy
     public void preDestroy() {
         log.info("in Game preDestroy");
-    }
-
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
     }
 
     @Override
