90ff6692c3efd2c4542735827003f5fe8bada752
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-21 20:43:22 +0300

    using properties file

--- 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/config/GameConfig.java
+++ 02-multi-module-spring-project/core/src/main/java/example/spring/multimodule/config/GameConfig.java
@@ -2,15 +2,20 @@ package example.spring.multimodule.config;
 
 import example.spring.multimodule.core.GuessCount;
 import example.spring.multimodule.core.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:game.properties")
 public class GameConfig {
 
-    private int maxNumber = 50;
+    @Value("${game.maxNumber:50}")
+    private int maxNumber;
 
-    private int guessCount = 10;
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     @Bean
     @MaxNumber
