60e7da69979a517ef24aad11a0474f7c97628926
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-24 20:10:36 +0300

    setup Dispatcher Servlet

---
+++ 03-spring-mvc/src/main/java/example/spring/mvc/config/WebConfig.java
@@ -0,0 +1,11 @@
+package example.spring.mvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "example.spring.mvc")
+public class WebConfig {
+}
