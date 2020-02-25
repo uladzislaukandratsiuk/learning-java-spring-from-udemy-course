91f70de387afcf753626e6f0fbf52d9098fdbec3
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-02-25 20:13:00 +0300

    simple view and view resolver

--- 03-spring-mvc/src/main/java/example/spring/mvc/controller/DemoController.java
+++ 03-spring-mvc/src/main/java/example/spring/mvc/controller/DemoController.java
@@ -12,4 +12,9 @@ public class DemoController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("welcome")
+    public String welcome() {
+        return "welcome";
+    }
 }
---
+++ 03-spring-mvc/src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Welcome on jsp page</title>
+</head>
+<body>
+<h1>Hello from jsp page!</h1>
+</body>
+</html>
