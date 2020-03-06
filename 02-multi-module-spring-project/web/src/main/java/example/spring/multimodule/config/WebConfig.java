ce9a0e7eb1e5a3931ebc7f4f65628769f0fd36a7
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-06 22:13:58 +0300

    Local Change Interceptor

--- 02-multi-module-spring-project/web/src/main/java/example/spring/multimodule/config/WebConfig.java
+++ 02-multi-module-spring-project/web/src/main/java/example/spring/multimodule/config/WebConfig.java
@@ -2,13 +2,22 @@ package example.spring.multimodule.config;
 
 import example.spring.multimodule.interceptor.RequestInterceptor;
 import example.spring.multimodule.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -18,5 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestInterceptor());
+
+        registry.addInterceptor(new LocaleChangeInterceptor());
     }
 }
