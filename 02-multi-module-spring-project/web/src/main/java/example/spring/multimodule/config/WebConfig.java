836eb282b42edc7112281386c9382253dc90a0ca
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-06 21:59:25 +0300

    Request Interception

--- 02-multi-module-spring-project/web/src/main/java/example/spring/multimodule/config/WebConfig.java
+++ 02-multi-module-spring-project/web/src/main/java/example/spring/multimodule/config/WebConfig.java
@@ -1,7 +1,9 @@
 package example.spring.multimodule.config;
 
+import example.spring.multimodule.interceptor.RequestInterceptor;
 import example.spring.multimodule.util.ViewNames;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
