ef8c69b53bae3c03c44757b966a2dc415625b275
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-03-07 19:44:21 +0300

    gradle spring boot first project

---
+++ 05-gradle-spring-boot/src/main/java/example/spring/gradle/GradleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package example.spring.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringBootApplication.class, args);
+	}
+
+}
---
+++ 05-gradle-spring-boot/src/test/java/example/spring/gradle/GradleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package example.spring.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
